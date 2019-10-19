49179ffe82edc5db84063e624dc04ef842f62c3f
fifacode <fifav@seznam.cz>
2019-10-19 13:19:41 -0700

    props

---
+++ fartgame-unity/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("horizontal"), 0f, Input.GetAxis("horizontal"));
+        move *= Speed * Time.deltaTime;
+        transform.Translate(move);
+    }
+}
